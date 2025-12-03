ec5f79760cb793bf2e7f892d2676a387861b42b6
MercelinSanthiya <mercelinsanthiya3105@gmail.com>
2025-12-03 15:48:03 +0530

    Removed folder

--- booking2/src/main/java/com/project/booking2/mordel/TripType.java
+++
@@ -1,6 +0,0 @@
-package com.project.booking2.mordel;
-
-public enum TripType {
-    ONE_WAY,
-    ROUND_TRIP
-}
--- booking2/src/main/java/com/project/booking2/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.booking2.repository;
-import com.project.booking2.entity.BookingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
-    List<BookingEntity> findByTripType(String tripType);
-}
